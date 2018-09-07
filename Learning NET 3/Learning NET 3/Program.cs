5c3392fd002d5245212e1369740078b2f5040156
Karol Kozuch <karol199714@gmail.com>
2018-09-07 20:28:05 +0200

    Hmph.Seems like there was no code to write in this tutorial...

---
+++ Learning NET 3/Learning NET 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning_NET_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NET learning 2/NET learning 2/Iface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET_learning_2
+{
+    interface IFace
+    {
+        void Hehe();
+        int Err { get; set; }
+    }
+    
+}
+
+abstract class CLAS
+{
+    abstract public void Lala();
+    public int a;
+    protected int Aa { get; set; }
+    event EventHandler NewEvent;
+}
